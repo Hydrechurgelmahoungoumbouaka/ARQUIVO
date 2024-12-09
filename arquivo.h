38b95d15b1fe20cf8327c6400ea452d9d15e1e18
Hydrech <urgelhydrech@gmail.com>
2024-12-09 00:33:16 -0300

    Update arquivo.h
    troca do valor

--- arquivo.h
+++ arquivo.h
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+void trocaValor(int *a,int *b){
+int aux;
+aux=*a;
+*a=*b;
+*b=aux;
+}
 int main()
 {
+    int num1=40;
+    int num2=20;
+    trocaValor(&num1,&num2);
+    printf("Num1 eh: %d\n",num1);
+    printf("Num2 eh: %d\n",num2);
 
     return 0;
 }
